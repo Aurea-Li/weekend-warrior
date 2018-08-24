8c30e2dbfe84aae170c0c406d73804c5c278e16f
Aurea <fu.sakura@gmail.com>
2018-08-24 15:46:33 -0700

    finish word_count

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,14 @@
+def words(sentence)
 
+words_lst = sentence.split(' ')
+
+word_cnt = {}
+words_lst.each do |word|
+  if word_cnt[word]
+    word_cnt[word] += 1
+  else
+    word_cnt[word] = 1
+  end
+end
+return word_cnt
+end
