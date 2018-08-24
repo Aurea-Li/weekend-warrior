655348e59242cc170dd17be859ed1695009868b7
Aurea <fu.sakura@gmail.com>
2018-08-24 12:07:04 -0700

    complete 02 robot name

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,21 @@
+class Robot
 
+  attr_reader :name
+
+  def initialize
+    @name = create_name
+  end
+
+  def reset
+    @name = create_name
+  end
+
+  def create_name
+
+    letter_list = [*'A'..'Z']
+    letters = letter_list[rand(26)] + letter_list[rand(26)]
+    name = letters + rand(10).to_s + rand(10).to_s + rand(10).to_s
+
+    return name
+  end
+end
